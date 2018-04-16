2ef0281d0b18c81c3905ee04deaa3cc12faf72f8
floatvoid <floatvoid@gmail.com>
2018-04-15 19:22:49 -0700

    stuff

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,42 +1,32 @@
-function Timeline(data)
-{
-  this.data = data;
+function Timeline(data) {
+    this.data = data;
 
-  this.parse = function(type)
-  {
-    var entriesText = this.data.trim().split("@");
-    var db = {};
-    for( var i=1; i<entriesText.length; i++ )
-    {
-        var lines = entriesText[i].trim().split("\n");
-        var entryMap = lines.map(liner); 
-        var entry = {};
-        for( e in entryMap)
-        {
-            for( k in entryMap[e])
-            {
-                entry[k] = entryMap[e][k];
-            }            
+    this.parse = function (type) {
+        var entriesText = this.data.trim().split("@");
+        var db = {};
+        for (var i = 1; i < entriesText.length; i++) {
+            var lines = entriesText[i].trim().split("\n");
+            var sub = false;
+            entry = {};
+            subList = [];
+            s = 0;
+            for (var l in lines) {
+                line = lines[l].trim();
+
+                if (line.startsWith('+')) {
+                    subList[s] = line.split(' ')[1].trim();
+                    s++;
+                }
+                else
+                {
+                    var k = line.split(': ')[0];
+                    var v = line.split(': ')[1];
+                    entry[k] = v;                        
+                }
+            }
+            entry["sub"] = subList;
+            db[i] = entry;
         }
-        
-        db[i] = entry;
-        console.log();
+        return db;
     }
-    return db;
-  }
-
-}
-
-function liner(line)
-{
-    if( line.trim().startsWith('+') )
-    {
-        console.log(line);
-    }
-    var key = line.indexOf(": ") > -1 ? line.split(": ")[0].trim() : null;
-    var value = line.indexOf(": ") > -1 ? line.split(": ")[1].trim() : null;
-    var e = {};
-    e[key] = value;
-   // console.log(key);
-    return e;
 }
