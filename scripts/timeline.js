a84a82e2266b666fbad25a7b4ce7c1cf80ba8e9a
floatvoid <floatvoid@gmail.com>
2018-04-16 00:28:40 -0700

    stuff

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -2,29 +2,32 @@ function Timeline(data) {
     this.data = data;
 
     this.parse = function (type) {
-        var entriesText = this.data.trim().split("@");
+        var entriesText = this.data.trim().split("DATE");
         var db = {};
         for (var i = 1; i < entriesText.length; i++) {
             var lines = entriesText[i].trim().split("\n");
+            lines[0] = "DATE" + lines[0];
             var sub = false;
             entry = {};
             subList = [];
             s = 0;
             for (var l in lines) {
                 line = lines[l].trim();
-
-                if (line.startsWith('+')) {
-                    subList[s] = line.split(' ')[1].trim();
+                                    
+                if (line.startsWith('+')||line.startsWith('-')) {
+                    var li = {};
+                    li.content = line.substr(2);
+                    li.mark = line[0]; 
+                    subList[s] = li;
                     s++;
                 }
-                else
-                {
+                else {
                     var k = line.split(': ')[0];
                     var v = line.split(': ')[1];
-                    entry[k] = v;                        
+                    entry[k] = v;
                 }
             }
-            entry["sub"] = subList;
+            entry["LIST"] = subList;
             db[i] = entry;
         }
         return db;
